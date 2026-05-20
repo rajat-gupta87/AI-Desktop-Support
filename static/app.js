463dc8763fe258e416671872b0759249aba901f3
Rajat Gupta <rajatgupta87799@gmail.com>
2026-05-20 22:57:39 +0530

    fixed login

--- static/app.js
+++ static/app.js
@@ -11,7 +11,52 @@ window.onload = () => {
     loadTickets();
 };
 
+async function login() {
 
+    const email =
+        document.getElementById(
+            "email"
+        ).value;
+
+    const password =
+        document.getElementById(
+            "password"
+        ).value;
+
+    const response = await fetch(
+        "/login",
+        {
+            method: "POST",
+
+            headers: {
+                "Content-Type":
+                    "application/json"
+            },
+
+            body: JSON.stringify({
+                email,
+                password
+            })
+        }
+    );
+
+    const data =
+        await response.json();
+
+    if (data.access_token) {
+
+        localStorage.setItem(
+            "token",
+            data.access_token
+        );
+
+        window.location.href = "/";
+
+    } else {
+
+        alert(data.message);
+    }
+}
 
 async function loadHistory() {
 
